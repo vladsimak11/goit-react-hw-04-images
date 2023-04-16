a6384d282d3abb674da1e6200f9f24fbb51012e5
Vladyslav Simak <simakv21@gmail.com>
2023-04-16 21:27:08 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import css from './ImageGallery.module.css';
 import {fetchImages} from '../../services/fetch';
 import {ImageGalleryItem} from '../ImageGalleryItem/ImageGalleryItem';
@@ -6,98 +6,79 @@ import {Button} from '../Button/Button';
 import {ThreeDots} from '../Loader/Loader';
 import {Modal} from '../Modal/Modal';
 
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    showModal: false,
-    largeImageURL: '',
-    currentPage: 1,
-  }
+export const ImageGallery = ({searchValue}) => {
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const {searchValue} = this.props;
-    prevState.currentPage = 1;
-    if(prevProps.searchValue !== searchValue && searchValue) {
-      
-      this.onResetState();
+  useEffect(() => {
+    if(searchValue) {
+      onResetState();
+      getImages();
+    }
+  }, [searchValue])
 
-      this.getImages();
-    } 
-
-  }
-
-  onResetState = () => {
-    this.setState({
-      images: [],
-      currentPage: 1,
-    });
-  }
-
-  getImages = () => {
-    this.setState({isLoading: true})
-      fetchImages(this.props.searchValue, this.state.currentPage)
+  const getImages = () => {
+    setIsLoading(true);
+      fetchImages(searchValue, currentPage)
         .then(({hits}) => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...hits],
-          currentPage: prevState.currentPage + 1,
-          }))
+          setImages((prevImages) => [...prevImages, ...hits]);
+          setCurrentPage((prevPage) => prevPage + 1);
         })
         .catch(error => console.log(error))
         .finally(() => {
-          this.setState({isLoading: false})
+          setIsLoading(false);
         });
 
   }
 
-  toggleModal = () => {
-    this.setState(({showModal}) => ({
-      showModal: !showModal
-    }))
+  const onResetState = () => {
+    setImages([]);
+    setCurrentPage(1)
   }
 
-  getLargeImg = (image) => {
-    this.setState({
-      largeImageURL: image,
-      showModal: true
-    })
+  const toggleModal = () => {
+    setShowModal(!showModal)
   }
 
-
-  render() {
-    const {images, isLoading, showModal, largeImageURL} = this.state;
-    const checkLoadMore = images.length > 0;
-    return (
-      <>
-        {!checkLoadMore && <div style={{
-          display: 'flex',
-          justifyContent: 'center'
-          }}>
-
-          <p className={css.block}>The gallery is empty. <br /> Enter the value in the search field!</p>
-          
-          </div>
-        }
-
-        {showModal && <Modal onClose = {this.toggleModal} >
-          <img src={largeImageURL} alt=''/>
-          </Modal>
-        }
-
-        {
-          images && (
-          <>
-          <ul className={css.imageGallery}>
-          <ImageGalleryItem images={images} getLargeImg={this.getLargeImg}/>
-          </ul>
-          {checkLoadMore && <Button onClick={this.getImages}/> }
-          <div className={css.loader}>
-            {isLoading && <ThreeDots /> }
-          </div>
-          </>
-          )
-        }
-      </>
-    )
+  const getLargeImg = (image) => {
+    setLargeImageURL(image);
+    setShowModal(true);
   }
+
+  const checkLoadMore = images.length > 0;
+  return (
+    <>
+      {!checkLoadMore && <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+        }}>
+
+        <p className={css.block}>The gallery is empty. <br /> Enter the value in the search field!</p>
+        
+        </div>
+      }
+
+      {showModal && <Modal onClose = {toggleModal} >
+        <img src={largeImageURL} alt=''/>
+        </Modal>
+      }
+
+      {
+        images && (
+        <>
+        <ul className={css.imageGallery}>
+        <ImageGalleryItem images={images} getLargeImg={getLargeImg}/>
+        </ul>
+        {checkLoadMore && <Button onClick={getImages}/> }
+        <div className={css.loader}>
+          {isLoading && <ThreeDots /> }
+        </div>
+        </>
+        )
+      }
+    </>
+  )
 }
